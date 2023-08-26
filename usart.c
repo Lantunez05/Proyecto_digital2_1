d8542272a86e7e4e0239a46f6f35b90d97e3b42a
sansancas <128657163+sansancas@users.noreply.github.com>
2023-08-26 14:04:53 -0600

    Add files via upload

---
+++ usart.c
@@ -0,0 +1,58 @@
+
+// PIC16F887 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG1
+#pragma config FOSC = EXTRC_CLKOUT// Oscillator Selection bits (RC oscillator: CLKOUT function on RA6/OSC2/CLKOUT pin, RC on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = OFF       // RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = OFF       // Brown Out Reset Selection bits (BOR enabled)
+#pragma config IESO = OFF        // Internal External Switchover bit (Internal/External Switchover mode is enabled)
+#pragma config FCMEN = OFF       // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is enabled)
+#pragma config LVP = OFF         // Low Voltage Programming Enable bit (RB3/PGM pin has PGM function, low voltage programming enabled)
+
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+#include "UART.h"
+
+#define _XTAL_FREQ 8000000 
+
+void txt_UART(char txt[]);
+
+uint8_t data;
+
+void __interrupt() isr(){
+    if(PIR1bits.RCIF){
+        data = RCREG;
+        //I2C_Write(0x50, data);
+        string("HOLA");
+        PIR1bits.RCIF = 0;
+        
+    }
+}
+
+void main(void) {
+    
+    OSCCONbits.IRCF = 0b111;
+    OSCCONbits.SCS = 1;
+    //OPTION_REGbits.PSA = 0;
+    //OPTION_REGbits.PS =  0;
+    INTCONbits.GIE = 1;
+    INTCONbits.PEIE = 1;
+    PIE1bits.RCIE = 1;
+    PIR1bits.RCIF = 0;
+    initUART(8);
+    while(1)
+        __delay_ms(1000);
+    return;
+}
